232d23e3df4480f7628159dc347ed66b48bbda35
sakibalamin <sakibalamin162@gmail.com>
2020-12-08 22:30:32 +0600

    functions added, not implemented

--- main.go
+++ main.go
@@ -4,6 +4,22 @@ import (
 	"fmt"
 )
 
+func add(x, y int) int {
+	return -1
+}
+
+func sub(x, y int) int {
+	return -1
+}
+
+func mul(x, y int) int {
+	return -1
+}
+
+func div(x, y int) int {
+	return -1
+}
+
 func main() {
 	fmt.Println("Git Stash!")
 }
